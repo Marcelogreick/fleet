d9260aabf215ade5cfd423507693d058f5b1621a
mgraires.gok <mgraires@rd.com.br>
2023-10-25 11:18:01 -0300

    init project

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
+import { styles } from "./styles";
 
-export default function App() {
+export default function SignIn() {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
