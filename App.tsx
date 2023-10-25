49c0121e52b260498c80a50da8e0275409b7ca5c
mgraires.gok <mgraires@rd.com.br>
2023-10-25 11:33:14 -0300

    adicionando e configurando styled components

--- App.tsx
+++ App.tsx
@@ -1,11 +1,34 @@
-import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
+import { ThemeProvider } from "styled-components/native";
+
+import theme from "./src/theme";
+
+import { StatusBar } from "react-native";
+import { SignIn } from "./src/pages/SignIn";
+import { Loading } from "./src/components/Loading";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <Loading />;
+  }
+
   return (
-    <View>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <SignIn />
+    </ThemeProvider>
   );
 }
---
+++ src/components/Loading/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components/native";
+import theme from "../../theme";
+
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ theme }) => theme.COLORS.BRAND_DARK};
+`;
+
+export const LoadIndicator = styled.ActivityIndicator.attrs(() => ({
+  color: theme.COLORS.BRAND_LIGHT,
+}))``;
