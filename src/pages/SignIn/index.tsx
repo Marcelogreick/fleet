8f70c478eef84ef23923f276384b54f55a4e9be1
Marcelogreick <mgreick25@gmail.com>
2023-10-26 15:20:41 -0300

    add ios

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,19 +1,62 @@
-import { ImageSourcePropType } from "react-native";
+import { Alert, ImageSourcePropType } from "react-native";
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
 
 import { Container, Slogan, Title } from "./styles";
-
 import backgroundImg from "../../assets/background.png";
-
 import { Button } from "../../components/Button";
 
+import { ANDROID_CLIENT_ID, IOS_CLIENT_ID } from "@env";
+import { useEffect, useState } from "react";
+
+WebBrowser.maybeCompleteAuthSession();
+
 export function SignIn() {
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  const [_, response, googleSignIng] = Google.useAuthRequest({
+    androidClientId: ANDROID_CLIENT_ID,
+    iosClientId: IOS_CLIENT_ID,
+    scopes: ["profile", "email"],
+  });
+
+  function handleGoogleSignIn() {
+    setIsAuthenticating(true);
+
+    googleSignIng().then((response) => {
+      if (response?.type !== "success") {
+        setIsAuthenticating(false);
+      }
+    });
+  }
+
+  useEffect(() => {
+    if (response?.type === "success") {
+      if (response.authentication?.idToken) {
+        fetch(
+          `https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${response.authentication.idToken}`
+        ).then((response) => response.json());
+      } else {
+        Alert.alert(
+          "Entrar",
+          "Não foi possível conectar-se a sua conta google."
+        );
+        setIsAuthenticating(false);
+      }
+    }
+  }, [response]);
+
   return (
     <Container source={backgroundImg as ImageSourcePropType}>
       <Title>Ignite Fleet</Title>
 
       <Slogan>Gestão de uso de veículos</Slogan>
 
-      <Button title="Entrar com Google" />
+      <Button
+        title="Entrar com Google"
+        onPress={handleGoogleSignIn}
+        isLoading={isAuthenticating}
+      />
     </Container>
   );
 }
