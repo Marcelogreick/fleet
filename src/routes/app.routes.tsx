889898163d82b04b321053c53754f69fad7b0ef1
Marcelogreick <mgreick25@gmail.com>
2023-10-27 11:40:39 -0300

    criando a rota

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,23 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import { Home } from "../pages/Home";
+// import { Departure } from '../pages/Departure';
+// import { Arrival } from '../pages/Arrival';
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+export function AppRoutes() {
+  return (
+    <Navigator screenOptions={{ headerShown: false }}>
+      <Screen name="home" component={Home} />
+      {/* <Screen 
+        name="departure"
+        component={Departure}
+      />
+      <Screen 
+        name="arrival"
+        component={Arrival}
+      /> */}
+    </Navigator>
+  );
+}
