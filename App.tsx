ee72685068bf0d707296a560e9f4e230dcbed1d7
Marcelogreick <mgreick25@gmail.com>
2023-10-28 16:02:05 -0300

    descobrir se ta online ou offline

--- App.tsx
+++ App.tsx
@@ -10,7 +10,7 @@ import { ThemeProvider } from "styled-components/native";
 import { AppProvider, UserProvider } from "@realm/react";
 
 import theme from "./src/theme";
-
+import { WifiSlash } from "phosphor-react-native";
 import { StatusBar } from "react-native";
 import { SignIn } from "./src/pages/SignIn";
 import { Loading } from "./src/components/Loading";
@@ -18,12 +18,16 @@ import { REALM_APP_ID } from "@env";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Routes } from "./src/routes";
 import { RealmProvider, syncConfig } from "./src/libs/realm";
+import { TopMessage } from "./src/components/TopMessage";
+import { useNetInfo } from "@react-native-community/netinfo";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
   });
+
+  const netInfo = useNetInfo();
 
   if (!fontsLoaded) {
     return <Loading />;
@@ -33,6 +37,10 @@ export default function App() {
     <AppProvider id={REALM_APP_ID}>
       <ThemeProvider theme={theme}>
         <SafeAreaProvider style={{ backgroundColor: theme.COLORS.GRAY_800 }}>
+          {!netInfo.isConnected && (
+            <TopMessage title="Você está off-line" icon={WifiSlash} />
+          )}
+
           <StatusBar
             barStyle="light-content"
             backgroundColor="transparent"
