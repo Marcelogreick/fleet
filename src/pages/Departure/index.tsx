564e632f2c2ddffcb2df0ac8894a8b6926acc82c
Marcelogreick <mgreick25@gmail.com>
2023-10-28 10:45:46 -0300

    cadastro

--- src/pages/Departure/index.tsx
+++ src/pages/Departure/index.tsx
@@ -4,15 +4,72 @@ import { Button } from "../../components/Button";
 import { LicensePlateInput } from "../../components/LicensePlateInput";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import { useRef, useState } from "react";
-import { ScrollView, TextInput } from "react-native";
+import { Alert, ScrollView, TextInput } from "react-native";
 import { TextAreaInput } from "../../components/TextAreaInput";
+import { licensePlateValidate } from "../../utils/licensePlateValidate";
+import { useRealm } from "../../libs/realm";
+import { useUser } from "@realm/react";
+import { useNavigation } from "@react-navigation/native";
+import { Historic } from "../../libs/realm/scheme/Historic";
 
 export function Departure() {
   const [description, setDescription] = useState("");
   const [licensePlate, setLicensePlate] = useState("");
+  const [isRegistering, setIsResgistering] = useState(false);
+  const [isLoadingLocation, setIsLoadingLocation] = useState(true);
+
+  const realm = useRealm();
+  const user = useUser();
+  const { goBack } = useNavigation();
 
   const descriptionRef = useRef<TextInput>(null);
   const licensePlateRef = useRef<TextInput>(null);
+
+  function handleDepartureRegister() {
+    try {
+      if (!licensePlateValidate(licensePlate)) {
+        licensePlateRef.current?.focus();
+        return Alert.alert(
+          "Placa inválida",
+          "A placa é inválida. Por favor, informa a placa correta."
+        );
+      }
+
+      if (description.trim().length === 0) {
+        descriptionRef.current?.focus();
+        return Alert.alert(
+          "Finalidade",
+          "Por favor, informe a finalidade da utilização do veículo"
+        );
+      }
+
+      setIsResgistering(true);
+
+      realm.write(() => {
+        realm.create(
+          "Historic",
+          Historic.generate({
+            user_id: user!.id,
+            license_plate: licensePlate,
+            description,
+            // coords:[{
+            //   latitude: currentCoords.latitude,
+            //   longitude: currentCoords.longitude,
+            //   timestamp: new Date().getTime()
+            // }]
+          })
+        );
+      });
+
+      Alert.alert("Saída", "Saída do veículo registrada com sucesso.");
+
+      goBack();
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Erro", "Não possível registrar a saída do veículo.");
+      setIsResgistering(false);
+    }
+  }
 
   return (
     <Container>
@@ -28,7 +85,7 @@ export function Departure() {
               descriptionRef.current?.focus();
             }}
             returnKeyType="next"
-            // onChangeText={setLicensePlate}
+            onChangeText={setLicensePlate}
           />
         </ScrollView>
       </KeyboardAwareScrollView>
@@ -37,13 +94,17 @@ export function Departure() {
         ref={descriptionRef}
         label="Finalizade"
         placeholder="Vou utilizar o veículo para..."
-        // onSubmitEditing={handleDepartureRegister}
+        onSubmitEditing={handleDepartureRegister}
         returnKeyType="send"
         blurOnSubmit
-        // onChangeText={setDescription}
+        onChangeText={setDescription}
       />
 
-      <Button title="Registar Saída" />
+      <Button
+        title="Registar Saída"
+        onPress={handleDepartureRegister}
+        isLoading={isRegistering}
+      />
     </Container>
   );
 }
--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,8 +1,7 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import { Home } from "../pages/Home";
-// import { Departure } from '../pages/Departure';
-// import { Arrival } from '../pages/Arrival';
+import { Departure } from "../pages/Departure";
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
@@ -10,11 +9,8 @@ export function AppRoutes() {
   return (
     <Navigator screenOptions={{ headerShown: false }}>
       <Screen name="home" component={Home} />
+      <Screen name="departure" component={Departure} />
       {/* <Screen 
-        name="departure"
-        component={Departure}
-      />
-      <Screen 
         name="arrival"
         component={Arrival}
       /> */}
