8b6f5a414e75fc43477422111fcad2f5507e2715
Marcelogreick <mgreick25@gmail.com>
2023-10-28 10:59:01 -0300

    page arrival

---
+++ src/pages/Arrival/index.tsx
@@ -0,0 +1,5 @@
+import { Container } from "./styles";
+
+export function Arrival() {
+  return <Container></Container>;
+}
--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -2,6 +2,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import { Home } from "../pages/Home";
 import { Departure } from "../pages/Departure";
+import { Arrival } from "../pages/Arrival";
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
@@ -10,10 +11,7 @@ export function AppRoutes() {
     <Navigator screenOptions={{ headerShown: false }}>
       <Screen name="home" component={Home} />
       <Screen name="departure" component={Departure} />
-      {/* <Screen 
-        name="arrival"
-        component={Arrival}
-      /> */}
+      <Screen name="arrival" component={Arrival} />
     </Navigator>
   );
 }
