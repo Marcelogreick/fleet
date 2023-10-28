17875c2b565ac56194bce32d7ec31c4058d64f8b
Marcelogreick <mgreick25@gmail.com>
2023-10-27 22:48:34 -0300

    criando outros componentes

---
+++ src/pages/Departure/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from "./styles";
+import { Header } from "../../components/Header";
+
+export function Departure() {
+  return (
+    <Container>
+      <Header title="Saída" />
+    </Container>
+  );
+}
