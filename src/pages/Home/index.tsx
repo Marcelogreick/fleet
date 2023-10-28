5bfb8ec46dfe7aee1df7db660fbdfd7fb3c1d935
Marcelogreick <mgreick25@gmail.com>
2023-10-28 10:57:00 -0300

    card status e navegation

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,22 +1,63 @@
-import React from "react";
+import { useUser } from "@realm/react";
+import { useQuery, useRealm } from "../../libs/realm";
 
 import { Container } from "./styles";
 import { CarStatus } from "../../components/CarStatus";
 import { HomeHeader } from "../../components/HomeHeader";
 import { useNavigation } from "@react-navigation/native";
+import { Historic } from "../../libs/realm/scheme/Historic";
+import { HistoricCardProps } from "../../components/HistoricCard";
+import { useEffect, useState } from "react";
+import { Alert } from "react-native";
+import { Title } from "../SignIn/styles";
 
 export function Home() {
+  const [vehicleInUse, setVehicleInUse] = useState<Historic | null>(null);
+  const [vehicleHistoric, setVehicleHistoric] = useState<HistoricCardProps[]>(
+    []
+  );
+
   const { navigate } = useNavigation();
 
+  const historic = useQuery(Historic);
+  const user = useUser();
+  const realm = useRealm();
+
   function handleRegisterMoviment() {
-    navigate("departure");
+    if (vehicleInUse?._id) {
+      navigate("arrival", { id: vehicleInUse._id.toString() });
+    } else {
+      navigate("departure");
+    }
   }
+
+  function fetchVehicleInUse() {
+    try {
+      const vehicle = historic.filtered("status='departure'")[0];
+      setVehicleInUse(vehicle);
+    } catch (error) {
+      Alert.alert(
+        "Veículo em uso",
+        "Não foi possível carregar o veículo em uso."
+      );
+      console.log(error);
+    }
+  }
+
+  // useEffect(() => {
+  //   fetchHistoric();
+  // },[historic]);
 
   return (
     <Container>
       <HomeHeader />
 
-      <CarStatus onPress={handleRegisterMoviment} />
+      <CarStatus
+        licensePlate={vehicleInUse?.license_plate}
+        onPress={handleRegisterMoviment}
+      />
+
+      <Title>Histórico</Title>
     </Container>
   );
 }
