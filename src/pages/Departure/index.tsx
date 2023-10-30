9c4998da21c296c0cf6fc4c6e834f0ed18ea29dc
Marcelogreick <mgreick25@gmail.com>
2023-10-30 11:58:03 -0300

    componente location info

--- src/pages/Departure/index.tsx
+++ src/pages/Departure/index.tsx
@@ -1,9 +1,9 @@
-import { Container, Content } from "./styles";
+import { Container, Content, Message, MessageContent } from "./styles";
 import { Header } from "../../components/Header";
 import { Button } from "../../components/Button";
 import { LicensePlateInput } from "../../components/LicensePlateInput";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Alert, ScrollView, TextInput } from "react-native";
 import { TextAreaInput } from "../../components/TextAreaInput";
 import { licensePlateValidate } from "../../utils/licensePlateValidate";
@@ -11,12 +11,29 @@ import { useRealm } from "../../libs/realm";
 import { useUser } from "@realm/react";
 import { useNavigation } from "@react-navigation/native";
 import { Historic } from "../../libs/realm/scheme/Historic";
+import { openSettings } from "../../utils/openSettings";
+import {
+  useForegroundPermissions,
+  requestBackgroundPermissionsAsync,
+  watchPositionAsync,
+  LocationAccuracy,
+  LocationSubscription,
+  LocationObjectCoords,
+} from "expo-location";
+import { Loading } from "../../components/Loading";
+import { getAddressLocation } from "../../utils/getAddressLocation";
 
 export function Departure() {
   const [description, setDescription] = useState("");
   const [licensePlate, setLicensePlate] = useState("");
   const [isRegistering, setIsResgistering] = useState(false);
   const [isLoadingLocation, setIsLoadingLocation] = useState(true);
+  const [currentAddress, setCurrentAddress] = useState<string | null>(null);
+  const [currentCoords, setCurrentCoords] =
+    useState<LocationObjectCoords | null>(null);
+
+  const [locationForegroundPermission, requestLocationForegroundPermission] =
+    useForegroundPermissions();
 
   const realm = useRealm();
   const user = useUser();
@@ -69,6 +86,63 @@ export function Departure() {
       Alert.alert("Erro", "Não possível registrar a saída do veículo.");
       setIsResgistering(false);
     }
+  }
+
+  useEffect(() => {
+    requestLocationForegroundPermission();
+  }, []);
+
+  useEffect(() => {
+    if (!locationForegroundPermission?.granted) {
+      return;
+    }
+
+    let subscription: LocationSubscription;
+
+    watchPositionAsync(
+      {
+        accuracy: LocationAccuracy.High,
+        timeInterval: 1000,
+      },
+      (location) => {
+        setCurrentCoords(location.coords);
+
+        getAddressLocation(location.coords)
+          .then((address) => {
+            if (address) {
+              setCurrentAddress(address);
+            }
+          })
+          .finally(() => setIsLoadingLocation(false));
+      }
+    ).then((response) => (subscription = response));
+
+    return () => {
+      if (subscription) {
+        subscription.remove();
+      }
+    };
+  }, [locationForegroundPermission?.granted]);
+
+  if (!locationForegroundPermission?.granted) {
+    return (
+      <Container>
+        <Header title="Saída" />
+        <MessageContent>
+          <Message>
+            Você precisa permitir que o aplicativo tenha acesso a localização
+            para acessar essa funcionalidade. Por favor, acesse as configurações
+            do seu dispositivo para conceder a permissão ao aplicativo.
+          </Message>
+
+          <Button title="Abrir configurações" onPress={openSettings} />
+        </MessageContent>
+      </Container>
+    );
+  }
+
+  if (isLoadingLocation) {
+    return <Loading />;
   }
 
   return (
